8d489e88ea5d7cba890c010454e4e36d5432748e
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-03 12:52:45 +0530

    Added necessary components.
    1. Added favicon,logo and font.
    2. Added Materialize.
    3. Updated 404 page.

--- public/js/fbinit.js
+++ public/js/fbinit.js
@@ -4,7 +4,7 @@
     authDomain: "rexband-admin-611bf.firebaseapp.com",
     databaseURL: "https://rexband-admin-611bf.firebaseio.com",
     projectId: "rexband-admin-611bf",
-    storageBucket: "rexband-admin-611bf.appspot.com",
+    storageBucket: "rexband-admin-611bf.appspot.com"
     };
     firebase.initializeApp(config);
 })(jQuery);
