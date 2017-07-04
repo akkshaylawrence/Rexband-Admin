2ff3fd32e1707be5605599d74948d8ed92e0e756
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-04 18:09:56 +0530

    Updated Login Page and Admin page.
    1. Added features to login page.
    2. Updated Layout of admin page.

--- public/8139878737/js/login.js
+++ public/8139878737/js/login.js
@@ -1,13 +1,25 @@
+window.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return;
+  }
+  switch (event.key) {
+    case "Enter":
+      logIn();
+      break;
+  }
+});
 function logIn() {
    var password = document.getElementById('password').value;
    var email = document.getElementById("email").value;
    if (password.length < 5) {
+      document.getElementById("password").focus();
       document.getElementById('password').classList.add("invalid");
       return;
    }
    document.getElementById('login').innerHTML = "LOGGING YOU IN";
    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
       console.log(error.message);
+      document.getElementById("password").focus();
       document.getElementById('password').classList.add("invalid");
       document.getElementById('login').innerHTML = "LOGIN";
    });
@@ -20,6 +32,7 @@ function logIn() {
 function logOut() {
    firebase.auth().signOut().then(function () {
       window.location = 'admin-login.html'
+      document.getElementById("email").focus();
    }).catch(function (error) {
       console.log(error.message);
    });
