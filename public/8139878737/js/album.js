13a1b9968ffeddd06695ef42d45b1ac834cdefcc
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-07 16:36:36 +0530

    Completed Album Page.

--- public/8139878737/js/album.js
+++ public/8139878737/js/album.js
@@ -1,97 +1,86 @@
 function addRow() {
-   var number = document.getElementById('nosong').value;
-   number = parseInt(number);
-   var container = document.getElementById("list");
-   while (container.hasChildNodes()) {
-      container.removeChild(container.lastChild);
-   }
-   for (i = 0; i < number; i++) {
-      var j = i + 1;
-      var div = document.createElement("div");
-      div.className = "row song" + (i + 1);
-      div.style = "margin-top: 10px;padding: 5px 20px 20px 20px;border: 1px solid #9e9e9e;"
-//
-      var div1 = document.createElement("div");
-      div1.className = "col.s12";
-//
-      var inputn = document.createElement("input");
-      inputn.type = "text";
-      inputn.id = "song" + j;
-//
-      var label = document.createElement("label");
-      label.for = "song" + j;
-      label.innerHTML = 'Enter song name';
-//
-      var inputy = document.createElement("input");
-      inputy.type = "text";
-      inputy.id = "songY" + j;
-//
-      var label1 = document.createElement("label");
-      label1.for = "songY" + j;
-      label1.innerHTML = 'Enter YouTube link';
-//
-      var div3 = document.createElement("div");
-      div3.className = "file-field input-field";
-//
-      var div4 = document.createElement("div");
-      div4.className = "btn";
-//
-      var div5 = document.createElement("div");
-      div5.className = "file-path-wrapper";
-//
-      var span = document.createElement("span");
-      span.innerHTML = "Browse"
-//
-      var inputlf = document.createElement("input");
-      inputlf.type = "file";
-      inputlf.id = "file" + j;
-//
-      var inputlfn = document.createElement("input");
-      inputlfn.type = "text";
-      inputlfn.id = "filename" + j;
-      inputlfn.placeholder="Lyrics Files";
-      inputlfn.className = "file-path validate"
-//
-      var div6 = document.createElement("div");
-      div6.className = "file-field input-field";
-//
-      var div7 = document.createElement("div");
-      div7.className = "btn";
-//
-      var div8 = document.createElement("div");
-      div8.className = "file-path-wrapper";
-//
-      var span1 = document.createElement("span");
-      span1.innerHTML = "Browse"
-//
-      var inputlf1 = document.createElement("input");
-      inputlf1.type = "file";
-      inputlf1.id = "file1" + j;
-//
-      var inputlfn1 = document.createElement("input");
-      inputlfn1.type = "text";
-      inputlfn1.id = "filename1" + j;
-      inputlfn1.placeholder="Chord Files";
-      inputlfn1.className = "file-path validate";
-//
-      container.appendChild(div);
-      div.appendChild(div1);
-      div1.appendChild(label);
-      div1.appendChild(inputn);
-      div1.appendChild(label1);
-      div1.appendChild(inputy);
-      div1.appendChild(div3);
-      div3.appendChild(div4);
-      div3.appendChild(div5);
-      div4.appendChild(span);
-      div4.appendChild(inputlf);
-      div5.appendChild(inputlfn);
-      div1.appendChild(div6);
-      div6.appendChild(div7);
-      div6.appendChild(div8);
-      div7.appendChild(span1);
-      div7.appendChild(inputlf1);
-      div8.appendChild(inputlfn1);
-   }
-   return;
+    var number = document.getElementById('nosong').value;
+    number = parseInt(number);
+    var container = document.getElementById("list");
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+    for (i = 0; i < number; i++) {
+        j = i + 1;
+        container.innerHTML += '<div class="row song1" style="margin-top: 10px; padding: 5px 20px 20px; border: 1px solid rgb(158, 158, 158);">\
+   <div class="input-field col.s12">\
+      <input type="text" id="song' + j + '">\
+      <label for="song' + j + '">Enter song name</label>\
+   </div>\
+   <div class="input-field col s12">\
+      <input type="text" id="songY' + j + '">\
+      <label for="songY' + j + '">Enter YouTube link</label>\
+   </div>\
+   <div class="input-field col s12">\
+      <div class="file-field input-field">\
+         <div class="btn">\
+            <span>Browse</span>\
+            <input type="file" id="file' + j + '"></div>\
+         <div class="file-path-wrapper">\
+            <input type="text" id="filename1" placeholder="Lyrics Files" class="file-path validate">\
+         </div>\
+      </div>\
+   </div>\
+   <div class="col s12">\
+      <div class="file-field input-field">\
+         <div class="btn">\
+            <span>Browse</span>\
+            <input type="file" id="file1' + j + '">\
+         </div>\
+         <div class="file-path-wrapper">\
+            <input type="text" id="filename11" placeholder="Chord Files" class="file-path validate">\
+         </div>\
+      </div>\
+   </div>\
+</div>\
+';
+    }
+    return;
 }
+albtn.addEventListener('click', function () {
+    var patha = 'Music/' + albumname.value;
+    database.ref(patha).set({
+        albumwriteup: albumtext.value,
+        youtubelink: ytube.value,
+        ituneslink: itunes.value,
+        soundcloudlink: soundc.value
+    });
+    var filename = document.getElementById('albumname');
+    var alb = albumname.value;
+    var file = document.getElementById('fileb').files[0];
+    var albumart = firebase.storage().ref('Albumart/' + alb);
+    var task = albumart.put(file);
+    var no = document.getElementById('nosong').value;
+    no = parseInt(no);
+    for (i = 1; i <= no; i++) {
+        s = 'song' + i;
+        var s = document.getElementById(s).value;
+        var pathh = 'Music/' + albumname.value + '/Songs/' + s;
+        var youtubelink = 'songY' + i;
+        youtubelink = document.getElementById(youtubelink).value;
+        database.ref(pathh).set({
+            Youtube: youtubelink
+        })
+    }
+    for (i = 1; i <= no; i++) {
+        var fileid = "file" + i;
+        var songl = "song" + i;
+        var songl = document.getElementById(songl).value;
+        var lyrfile = document.getElementById(fileid).files[0];
+        var lyrics = firebase.storage().ref('Lyrics/' + songl);
+        var task = lyrics.put(lyrfile);
+    }
+    for (i = 1; i <= no; i++) {
+        var fileid = "file1" + i;
+        var songc = "song" + i;
+        var songc = document.getElementById(songc).value;
+        var chordfile = document.getElementById(fileid).files[0];
+        var chords = firebase.storage().ref('Chords/' + songc);
+        var task = chords.put(chordfile);
+    }
+});
