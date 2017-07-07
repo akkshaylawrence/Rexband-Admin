759fd09436d4580735a7b150fea4ecbf50057b0e
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-07 11:42:41 +0530

    Removed Unnecessary files.

--- public/8139878737/js/album.js
+++ public/8139878737/js/album.js
@@ -6,55 +6,74 @@ function addRow() {
       container.removeChild(container.lastChild);
    }
    for (i = 0; i < number; i++) {
+      var j = i + 1;
       var div = document.createElement("div");
       div.className = "row song" + (i + 1);
       div.style = "margin-top: 10px;padding: 5px 20px 20px 20px;border: 1px solid #9e9e9e;"
+//
       var div1 = document.createElement("div");
       div1.className = "col.s12";
+//
       var inputn = document.createElement("input");
       inputn.type = "text";
-      inputn.id = "song" + (i + 1);
+      inputn.id = "song" + j;
+//
+      var label = document.createElement("label");
+      label.for = "song" + j;
+      label.innerHTML = 'Enter song name';
+//
       var inputy = document.createElement("input");
       inputy.type = "text";
-      inputy.id = "songY" + (i + 1);
-      var label = document.createElement("label");
-      label.for = "song" + (i + 1);
-      label.innerHTML = 'Enter song name'
+      inputy.id = "songY" + j;
+//
       var label1 = document.createElement("label");
-      label1.for = "songY" + (i + 1);
-      label1.innerHTML = 'Enter YouTube link'
+      label1.for = "songY" + j;
+      label1.innerHTML = 'Enter YouTube link';
+//
       var div3 = document.createElement("div");
       div3.className = "file-field input-field";
+//
       var div4 = document.createElement("div");
       div4.className = "btn";
+//
       var div5 = document.createElement("div");
       div5.className = "file-path-wrapper";
+//
       var span = document.createElement("span");
       span.innerHTML = "Browse"
+//
       var inputlf = document.createElement("input");
       inputlf.type = "file";
-      inputlf.id = "file" + (i + 1);
+      inputlf.id = "file" + j;
+//
       var inputlfn = document.createElement("input");
       inputlfn.type = "text";
-      inputlfn.id = "filename" + (i + 1);
+      inputlfn.id = "filename" + j;
       inputlfn.placeholder="Lyrics Files";
       inputlfn.className = "file-path validate"
+//
       var div6 = document.createElement("div");
       div6.className = "file-field input-field";
+//
       var div7 = document.createElement("div");
       div7.className = "btn";
+//
       var div8 = document.createElement("div");
       div8.className = "file-path-wrapper";
+//
       var span1 = document.createElement("span");
       span1.innerHTML = "Browse"
+//
       var inputlf1 = document.createElement("input");
       inputlf1.type = "file";
-      inputlf1.id = "file1" + (i + 1);
+      inputlf1.id = "file1" + j;
+//
       var inputlfn1 = document.createElement("input");
       inputlfn1.type = "text";
-      inputlfn1.id = "filename1" + (i + 1);
+      inputlfn1.id = "filename1" + j;
       inputlfn1.placeholder="Chord Files";
       inputlfn1.className = "file-path validate";
+//
       container.appendChild(div);
       div.appendChild(div1);
       div1.appendChild(label);
