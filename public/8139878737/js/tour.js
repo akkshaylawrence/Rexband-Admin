edf58c79e747e0155dc3ca49ae5de4a6c3945623
Akkshay Lawrence <cruzer6ryc4@gmail.com>
2017-07-09 17:32:57 +0530

    Added support to add multiple youtube links.

--- public/8139878737/js/tour.js
+++ public/8139878737/js/tour.js
@@ -1,9 +1,26 @@
+var i = 1;
 $('.datepicker').pickadate({
    selectMonths: true,
    selectYears: 15
 });
-document.getElementById("fileb").addEventListener('click',addTour,false);
-function addTour(){
+document.getElementById("fileb").addEventListener('click', addTour, false);
+document.getElementById("addlink").addEventListener('mousedown', addLink, false);
+function addTour() {
    document.getElementById("trbtn").classList.remove("disabled");
-   document.getElementById("trbtn").style.display="block";
+   document.getElementById("trbtn").style.display = "block";
+}
+function addLink() {
+   var container = document.getElementById("ylist");
+   container.innerHTML +=
+      '<div class="row">\
+         <div class="col s1">\
+            <i class="fa fa-youtube-play fa-2x"></i>\
+         </div>\
+         <div class="input-field col s10">\
+            <input id="ytube' + i +'" type="text" class="validate">\
+            <label data-error="Invalid Link!" for="ytube">YouTube Link</label>\
+         </div>\
+      </div>\
+   ';
+   i+=1;
 }
